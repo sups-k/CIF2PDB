664b32980a41073014b39cbe8e171be8fa813c10
sups-k <49182287+sups-k@users.noreply.github.com>
2019-08-01 16:36:39 +0530

    Added code for conversion
    Converts fractional to orthogonal coordinates but still cannot write to PDB correctly.

--- cif2pdb.R
+++ cif2pdb.R
@@ -1,3 +1,4 @@
+# install.packages("reader") if not extant
 # Clear variables
 rm(list=ls())
 
@@ -7,7 +8,7 @@ library("reader")
 ############# Getting a, b, c, alpha, beta, gamma ##########################
 
 # Reading the lines containing info from cif file (lines 68-73)
-raw_data = n.readLines(paste("/home/ibab/Bioinfo/293146.cif", sep = ""),
+raw_data = n.readLines(paste("293146.cif", sep = ""),
             header = FALSE, skip = 67, n = 6)
 # Converting the lines into a table
 raw_data_table = read.table(textConnection(raw_data))
@@ -29,7 +30,7 @@ gamma = data[6]
 
 ################ Getting fractional coordinates ######################
 # Reading the lines containing info from cif file (lines 187-225)
-raw_coord = n.readLines(paste("/home/ibab/Bioinfo/293146.cif", sep = ""),
+raw_coord = n.readLines(paste("293146.cif", sep = ""),
                        header = FALSE, skip = 186, n = 39)
 # Converting the lines into a table
 raw_coord_table = read.table(textConnection(raw_coord))
@@ -41,15 +42,34 @@ raw_coord_table$V5 = gsub("\\s*\\([^\\)]+\\)","",as.character(raw_coord_table$V5
 x_coord_extracted = as.data.frame(raw_coord_table[,3], drop=FALSE)
 y_coord_extracted = as.data.frame(raw_coord_table[,4], drop=FALSE)
 z_coord_extracted = as.data.frame(raw_coord_table[,5], drop=FALSE)
+atoms = as.data.frame(raw_coord_table[,2], drop=FALSE)
 # Converting the data frame into matrix
 x_coord = as.matrix(x_coord_extracted)
 y_coord = as.matrix(y_coord_extracted)
 z_coord = as.matrix(z_coord_extracted)
+atoms = as.matrix(atoms)
+colnames(atoms) <- "Atoms"
 # Converting the matrix into a numeric vector
 x_coord = as.numeric(x_coord)
 y_coord = as.numeric(y_coord)
 z_coord = as.numeric(z_coord)
 
-coord = cbind(x_coord, y_coord, z_coord)
+coord = rbind(x_coord, y_coord, z_coord)
 
 ######### Converting fractional to orthogonal ################
+cos_alpha = round(cos(alpha*(pi/180)), digits = 4)
+cos_beta = round(cos(beta*(pi/180)), digits = 4)
+cos_gamma = round(cos(gamma*(pi/180)), digits = 4)
+sin_gamma = round(sin(gamma*(pi/180)), digits = 4)
+
+v = round(sqrt(1 - (cos_alpha^2) - (cos_beta^2) - (cos_gamma^2) + (2 * cos_alpha * cos_beta * cos_gamma)), digits = 4)
+conversion_matrix = matrix(c(a, (b * cos_gamma), (c * cos_beta), 0, (b * sin_gamma), (c * (cos_alpha - (cos_beta * cos_gamma)) / sin_gamma),
+                             0, 0, (c * v / sin_gamma)),
+                           nrow = 3, ncol = 3, byrow = TRUE)
+orth = matrix(rep(0, each = 3), nrow = ncol(coord), ncol = 3)
+colnames(orth) <- c("x", "y", "z")
+for(i in 1:ncol(coord)){
+  orth[i,] = round(conversion_matrix %*% coord[,i], digits = 3)
+}
+pdb = cbind.data.frame(atoms,orth)
+# write.table(pdb, file = "mypdb.txt", sep = "\t", row.names = FALSE, col.names = FALSE)
